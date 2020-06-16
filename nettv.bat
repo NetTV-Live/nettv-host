d0b0ec79d9bd44f09ac8b3b8ccfaeb2d5c015efc
Nettv.live <52923326+NetTV-Live@users.noreply.github.com>
2020-06-16 20:39:07 +0800

    Add files via upload
    下載後雙擊運行即可，若遇到安全軟件攔截，請選擇放行即可！

---
+++ nettv.bat
@@ -0,0 +1,4 @@
+attrib -R C:\WINDOWS\system32\drivers\etc\hosts 
+@echo 103.55.26.214 www.nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts
+@echo 103.55.26.214 radio.nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts 
+@echo 103.55.26.214 nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts
