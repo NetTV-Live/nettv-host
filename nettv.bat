c22eb01a0bc620141c1bb97e746156d282a39626
Nettv.live <52923326+NetTV-Live@users.noreply.github.com>
2021-12-24 21:58:56 +0800

    Update nettv.bat

--- nettv.bat
+++ nettv.bat
@@ -1,4 +1,4 @@
 attrib -R C:\WINDOWS\system32\drivers\etc\hosts 
-@echo 103.138.80.158 www.nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts
-@echo 103.138.80.158 radio.nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts 
-@echo 103.138.80.158 nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts
+@echo 103.138.80.201 www.nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts
+@echo 103.138.80.201 radio.nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts 
+@echo 103.138.80.201 nettv.live >>C:\WINDOWS\system32\drivers\etc\hosts
